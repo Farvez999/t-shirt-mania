6612d3b64fc3bb6574b33d5cddbee8ce56c672c3
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-09 00:23:19 +0600

    Simple Introduction to Context API to avoid Prop drilling

--- src/components/MySelf/MySelf.js
+++ src/components/MySelf/MySelf.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MyContext } from '../GrandPa/GrandPa';
 
 const MySelf = ({ house }) => {
+    const ring = useContext(MyContext)
+
     return (
         <div>
             <h4>My Self</h4>
             <p><small>House: {house}</small></p>
+            <p><small>Gift: {ring}</small></p>
         </div>
     );
 };
