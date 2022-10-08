d0b6d62df230c57b5081f3dfa778884bd330af6a
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-08 23:21:09 +0600

    Remove from cart and not allow duplicate

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
 import TShirt from '../TShirt/TShirt';
@@ -6,18 +6,41 @@ import './Home.css'
 
 const Home = () => {
     const tshirts = useLoaderData();
+
+    const [cart, setCart] = useState([])
+
+    const handleAddToCart = (tshirt) => {
+        const exists = cart.find(ts => ts._id === tshirt._id);
+        if (exists) {
+            alert('This item is already added')
+        } else {
+            const newCart = [...cart, tshirt]
+            setCart(newCart)
+            // alert('This item is added successful')
+        }
+    }
+
+    const handleRemoveItem = tshirt => {
+        const remaining = cart.filter(ts => ts._id !== tshirt._id);
+        setCart(remaining);
+    }
+
     return (
         <div className='home-container'>
             <div className="tshirt-container">
                 {
                     tshirts.map(tshirt => <TShirt
-                        key={tshirt.id}
+                        key={tshirt._id}
                         tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
                     ></TShirt>)
                 }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart
+                    cart={cart}
+                    handleRemoveItem={handleRemoveItem}
+                ></Cart>
             </div>
         </div>
     );
--- src/components/TShirt/TShirt.js
+++ src/components/TShirt/TShirt.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './TShirt.css'
 
-const TShirt = ({ tshirt }) => {
+const TShirt = ({ tshirt, handleAddToCart }) => {
     const { id, name, picture, price } = tshirt;
     return (
         <div className='t-shirt'>
             <img src={picture} alt="" />
             <h2>{name}</h2>
             <h4>Price: ${price}</h4>
-            <button>Buy Now</button>
+            <button onClick={() => handleAddToCart(tshirt)}>Buy Now</button>
         </div>
     );
 };
